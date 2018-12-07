4522a875c5e2d2452fc8965ca9bb7f0746303eff
ybcsie <ybcsie@gmail.com>
2018-12-07 08:30:34 +0000

    fix cookie locale name conflict under ncku domain

--- public/js/locale.js
+++ public/js/locale.js
@@ -7,25 +7,25 @@ var userLangCode = userLang.split('-')[0]
 var locale = $('.ui-locale')
 var supportLangs = []
 $('.ui-locale option').each(function () {
-  supportLangs.push($(this).val())
+	supportLangs.push($(this).val())
 })
-if (Cookies.get('locale')) {
-  lang = Cookies.get('locale')
-  if (lang === 'zh') {
-    lang = 'zh-TW'
-  }
+if (Cookies.get('locale_nc')) {
+	lang = Cookies.get('locale_nc')
+	if (lang === 'zh') {
+		lang = 'zh-TW'
+	}
 } else if (supportLangs.indexOf(userLang) !== -1) {
-  lang = supportLangs[supportLangs.indexOf(userLang)]
+	lang = supportLangs[supportLangs.indexOf(userLang)]
 } else if (supportLangs.indexOf(userLangCode) !== -1) {
-  lang = supportLangs[supportLangs.indexOf(userLangCode)]
+	lang = supportLangs[supportLangs.indexOf(userLangCode)]
 }
 
 locale.val(lang)
 $('select.ui-locale option[value="' + lang + '"]').attr('selected', 'selected')
 
 locale.change(function () {
-  Cookies.set('locale', $(this).val(), {
-    expires: 365
-  })
-  window.location.reload()
+	Cookies.set('locale_nc', $(this).val(), {
+		expires: 365
+	})
+	window.location.reload()
 })
